80e2e44fdf14014ce9f9f60066b9849be2de3c14
nickfranklin <nthompsonfranklin@gmail.com>
2017-05-23 15:31:03 -0400

    updated demonstrations

--- model/simulate_tools.py
+++ model/simulate_tools.py
@@ -2,6 +2,10 @@ import copy
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from matplotlib import gridspec
 from tqdm import tqdm
 
 from model import make_task, JointClustering, IndependentClusterAgent, FlatControlAgent
@@ -10,9 +14,8 @@ from model import MapClusteringAgent
 
 # Define a function to Simulate the Models
 def simulate_one(agent_class, simulation_number, task_kwargs, agent_kwargs=None, pruning_threshold=1000):
-    _kwargs = copy.copy(task_kwargs)
-    del _kwargs['list_goal_priors']
-    task = make_task(**_kwargs)
+
+    task = make_task(**task_kwargs)
     if agent_kwargs is not None:
         agent = agent_class(task, **agent_kwargs)
     else:
@@ -45,56 +48,10 @@ def simulate_task(n_sim, task_kwargs, agent_kwargs=None, alpha=2.0, pruning_thre
     results_ic = pd.concat(results_ic)
     results_fl = pd.concat(results_fl)
 
-    results_jc['Model'] = ['Joint Clusterting'] * len(results_jc)
+    results_jc['Model'] = ['Joint Clustering'] * len(results_jc)
     results_ic['Model'] = ['Independent Clustering'] * len(results_ic)
     results_fl['Model'] = ['Flat Agent'] * len(results_fl)
     return pd.concat([results_jc, results_ic, results_fl])
-
-
-def simulate_evaluate_map(agent_class, simulation_number, task_kwargs, agent_kwargs=None):
-    _kwargs = copy.copy(task_kwargs)
-    del _kwargs['list_goal_priors']
-    task = make_task(**_kwargs)
-    if agent_kwargs is not None:
-        agent = agent_class(task, **agent_kwargs)
-    else:
-        agent = agent_class(task)
-
-    results = agent.generate(evaluate_map_estimate=True)
-    results['Simulation Number'] = [simulation_number] * len(results)
-    results['Cumulative Steps Taken'] = results['n actions taken'].cumsum()
-
-    return results
-
-
-def simulate_task_with_map_control(n_sim, task_kwargs, agent_kwargs=None, alpha=2.0):
-    if agent_kwargs is None:
-        agent_kwargs = dict(alpha=alpha)
-    elif 'alpha' not in agent_kwargs.keys():
-        agent_kwargs['alpha'] = alpha
-
-    results_jc = [None] * n_sim
-    results_ic = [None] * n_sim
-    results_mc = [None] * n_sim
-    results_fl = [None] * n_sim
-
-    for ii in tqdm(range(n_sim)):
-        results_jc[ii] = simulate_one(JointClustering, ii, task_kwargs, agent_kwargs)
-        results_ic[ii] = simulate_one(IndependentClusterAgent, ii, task_kwargs, agent_kwargs)
-        results_fl[ii] = simulate_one(FlatControlAgent, ii, task_kwargs)
-        results_mc[ii] = simulate_one(MapClusteringAgent, ii, task_kwargs)
-
-    results_jc = pd.concat(results_jc)
-    results_ic = pd.concat(results_ic)
-    results_fl = pd.concat(results_fl)
-    results_mc = pd.concat(results_mc)
-
-    results_jc['Model'] = ['Joint Clusterting'] * len(results_jc)
-    results_ic['Model'] = ['Independent Clustering'] * len(results_ic)
-    results_fl['Model'] = ['Flat Agent'] * len(results_fl)
-    results_mc['Model'] = ['Map Clustering'] * len(results_mc)
-
-    return pd.concat([results_jc, results_ic, results_fl, results_mc])
 
 
 def list_entropy(_list):
@@ -115,3 +72,41 @@ def mutual_information(list_a, list_b):
         p_xy = np.sum([p == pi for pi in pairs], dtype=float) / len(pairs)
         h_xy += -p_xy * np.log2(p_xy)
     return h_x + h_y - h_xy
+
+
+def plot_results(df):
+    with sns.axes_style('ticks'):
+
+        _ = plt.figure(figsize=(9, 4))
+        gs = gridspec.GridSpec(1, 2, width_ratios=[1.8, 1], wspace=0.4)
+        ax0 = plt.subplot(gs[0])
+        ax1 = plt.subplot(gs[1])
+
+        # define the parameters to plot the results
+        # cc = sns.color_palette("Set 2")
+        tsplot_kwargs = dict(
+            time='TrialNumber',
+            value='Cumulative Steps Taken',
+            data=df[df['In goal']],
+            unit='Simulation Number',
+            condition='Model',
+            estimator=np.median,
+            ax=ax0,
+            color="Set2",
+            # order=["Independent Clustering", "Joint Clustering", "Flat Agent"]
+        )
+
+        sns.tsplot(**tsplot_kwargs)
+
+        df0 = df[df['In goal'] & (df.TrialNumber == df.TrialNumber.max())].copy()
+        df0.loc[df0.Model == 'Independent Clustering', 'Model'] = "Independent"
+        df0.loc[df0.Model == 'Joint Clustering', 'Model'] = "Joint"
+        df0.loc[df0.Model == 'Flat Agent', 'Model'] = "Flat"
+
+        sns.violinplot(data=df0, x='Model', y='Cumulative Steps Taken', ax=ax1, palette='Set2',
+                       # order=["Independent", "Joint", "Flat"]
+                       )
+        _, ub = ax1.get_ylim()
+        ax1.set_ylim([0, ub])
+
+        sns.despine(offset=5)
